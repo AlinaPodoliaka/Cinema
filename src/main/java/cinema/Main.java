09a9830ecd29d20aa9a3f5514e332a7dfb3e22a9
AlinaPodoliaka <57722396+AlinaPodoliaka@users.noreply.github.com>
2020-02-04 22:32:05 +0200

    added MovieSession added CinemaHall

--- src/main/java/cinema/Main.java
+++ src/main/java/cinema/Main.java
@@ -1,8 +1,16 @@
 package cinema;
 
 import cinema.lib.Injector;
+import cinema.model.CinemaHall;
 import cinema.model.Movie;
+import cinema.model.MovieSession;
+import cinema.service.CinemaHallService;
 import cinema.service.MovieService;
+import cinema.service.MovieSessionService;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Main {
     private static Injector injector = Injector.getInstance("cinema");
@@ -12,7 +20,24 @@ public class Main {
         movie.setTitle("Fast and Furious");
         MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
         movieService.add(movie);
+        System.out.println(movieService.getAll());
 
-        movieService.getAll().forEach(System.out::println);
+        CinemaHallService cinemaHallService = (CinemaHallService)
+                injector.getInstance(CinemaHallService.class);
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setCapacity(100);
+        cinemaHall = cinemaHallService.add(cinemaHall);
+        System.out.println(cinemaHallService.getAll());
+
+        MovieSession movieSession = new MovieSession();
+        movieSession.setCinemaHall(cinemaHall);
+        movieSession.setMovie(movie);
+        movieSession.setShowTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(19, 30)));
+        MovieSessionService movieSessionService = (MovieSessionService)
+                injector.getInstance(MovieSessionService.class);
+        movieSessionService.add(movieSession);
+        movieSessionService.findAvailableSessions(movie.getId(), LocalDate.now())
+                .forEach(System.out::println);
+
     }
 }
