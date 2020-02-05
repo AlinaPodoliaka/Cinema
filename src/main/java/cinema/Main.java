511f218c5e023ba1f793d052d5f1358018982560
AlinaPodoliaka <57722396+AlinaPodoliaka@users.noreply.github.com>
2020-02-05 14:53:49 +0200

    added MovieSession (#3)
    added CinemaHall

--- src/main/java/cinema/Main.java
+++ src/main/java/cinema/Main.java
@@ -1,8 +1,12 @@
 package cinema;
 
 import cinema.lib.Injector;
+import cinema.model.CinemaHall;
 import cinema.model.Movie;
+import cinema.model.MovieSession;
+import cinema.service.CinemaHallService;
 import cinema.service.MovieService;
+import cinema.service.MovieSessionService;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -17,6 +21,23 @@ public class Main {
         MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
         movieService.add(movie);
         System.out.println(movieService.getAll());
+
+        CinemaHallService cinemaHallService = (CinemaHallService)
+                injector.getInstance(CinemaHallService.class);
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setCapacity(100);
+        cinemaHall = cinemaHallService.add(cinemaHall);
+
+        MovieSession movieSession = new MovieSession();
+        movieSession.setCinemaHall(cinemaHall);
+        movieSession.setMovie(movie);
+        movieSession.setShowTime(LocalDateTime.of(LocalDate.now(), LocalTime.of(19, 30)));
+        MovieSessionService movieSessionService = (MovieSessionService)
+                injector.getInstance(MovieSessionService.class);
+        movieSessionService.add(movieSession);
+
+        movieSessionService.findAvailableSessions(movie.getId(), LocalDate.now())
+                .forEach(System.out::println);
 
     }
 }
---
+++ src/main/java/cinema/dao/impl/MovieSessionDaoImpl.java
@@ -0,0 +1,54 @@
+package cinema.dao.impl;
+
+import cinema.dao.MovieSessionDao;
+import cinema.exceptions.DataProcessingException;
+import cinema.lib.Dao;
+import cinema.model.MovieSession;
+import cinema.util.HibernateUtil;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class MovieSessionDaoImpl implements MovieSessionDao {
+    @Override
+    public MovieSession add(MovieSession session) {
+        Transaction transaction = null;
+        try (Session sessionH = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = sessionH.beginTransaction();
+            Long itemId = (Long) sessionH.save(session);
+            transaction.commit();
+            session.setId(itemId);
+            return session;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert Movie Session ", e);
+        }
+    }
+
+    @Override
+    public List<MovieSession> findAvailableSessions(Long movieId, LocalDate date) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<MovieSession> cq = cb.createQuery(MovieSession.class);
+            Root<MovieSession> root = cq.from(MovieSession.class);
+            Predicate predicateId = cb.equal(root.get("movie"), movieId);
+            Predicate predicateDate = cb.between(root.get("showTime"),
+                    date.atStartOfDay(), date.plusDays(1).atStartOfDay());
+            cq.select(root).where(cb.and(predicateId, predicateDate));
+            return session.createQuery(cq).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get all movies", e);
+        }
+    }
+}
